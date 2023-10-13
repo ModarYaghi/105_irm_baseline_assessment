930b62c9ede6b68cc60980f35114f9899e83e6b4
ModarYaghi <mfyaghi81@gmail.com>
2023-10-13 18:08:05 +0300

    working on variables name

---
+++ var_names.py
@@ -0,0 +1,56 @@
+qul_life = [
+    'qual_life1',
+    'qual_life2',
+    'qual_life3',
+    'qual_life4',
+    'qual_life5',
+    'qual_life6',
+    'qual_life7',
+    'qual_life8',
+    'qual_life9',
+    'qual_life10',
+    'qual_life11',
+    'qual_life12',
+    'qual_life13',
+    'qual_life14',
+    'qual_life15',
+    'qual_life16',
+    'qual_life17',
+    'qual_life18',
+    'qual_life19',
+    'qual_life23',
+    'qual_life24',
+    'qual_life25',
+    'qual_life26'
+
+]
+
+co_creation = [
+    'co_creation1',
+    'co_creation2',
+    'co_creation3',
+    'co_creation4',
+
+]
+
+concept = [
+    'concept1',
+    'concept2',
+    'concept3',
+    'concept4',
+    'concept5',
+    'concept6',
+
+]
+
+ptsd = [
+    'ptsd1',
+    'ptsd2',
+    'ptsd3',
+    'ptsd4',
+    'ptsd5',
+    'ptsd6',
+    'ptsd7',
+    'ptsd8'
+
+]
